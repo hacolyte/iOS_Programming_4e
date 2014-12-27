cc91b3905e0fb2059fdb750a7e699ef6a08d4d80
hacolyte <hacolytemail@gmail.com>
2014-12-27 04:09:21 -0500

    add quiz

---
+++ RandomItems/RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Benjamin Pearson on 12/27/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
