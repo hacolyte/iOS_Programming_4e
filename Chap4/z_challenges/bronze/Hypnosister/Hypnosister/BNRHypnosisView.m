18adce0b4e09de0393d55f1801d2cfd7b91ce08f
hacolyte <hacolytemail@gmail.com>
2015-01-02 18:01:09 -0500

    add bronze challenge to chapter 4

---
+++ Chap4/z_challenges/bronze/Hypnosister/Hypnosister/BNRHypnosisView.m
@@ -0,0 +1,66 @@
+//
+//  BNRHypnosisView.m
+//  Hypnosister
+//
+//  Created by Benjamin Pearson on 12/31/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRHypnosisView.h"
+
+@implementation BNRHypnosisView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // All BNRHypnosisViews start with a clear background color
+        self.backgroundColor = [UIColor clearColor];
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    CGRect bounds = self.bounds;
+    
+    // Figure out the center of the bounds rectangle
+    CGPoint center;
+    
+    center.x = bounds.origin.x + bounds.size.width / 2.0;
+    center.y = bounds.origin.y + bounds.size.height / 2.0;
+    
+    // The largest circle will circumscribe the view
+    float maxRadius = hypotf(bounds.size.width, bounds.size.height) / 2;
+    
+    UIBezierPath *path = [[UIBezierPath alloc] init];
+    
+    for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
+        // Move the point so that there is not a line connecting the circles
+        [path moveToPoint:CGPointMake(center.x + currentRadius, center.y)];
+        
+        [path addArcWithCenter:center
+                        radius:currentRadius
+                    startAngle:0.0
+                      endAngle:M_PI * 2.0
+                     clockwise:YES];
+    }
+    
+    // Configure line width to 10 points
+    path.lineWidth = 10;
+    
+    // Configure the drawing color to light gray
+    [[UIColor lightGrayColor] setStroke];
+    
+    // Draw the line!
+    [path stroke];
+    
+    // Import image
+    UIImage *logoImage = [UIImage imageNamed:@"logo.png"];
+    
+    [logoImage drawInRect:bounds];
+    
+}
+
+@end
